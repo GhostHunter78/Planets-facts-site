6642fb7fc90b2285e4b20c8b11af5028f957f807
Luka Talakvadze <lukatalakvadze@gmail.com>
2024-02-01 10:34:46 +0400

    corrected starting navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import "./style.css";
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import styled from "styled-components";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 
 function App() {
   return (
     <Main>
       <Header />
       <Routes>
-        {/* <Route path="/" element={<Navigate to="/planets/mercury" />} /> */}
+        <Route path="/" element={<Navigate to="/planets/mercury" />} />
         <Route path="/planets/:name" element={<Home />} />
       </Routes>
     </Main>
   );
 }
 
-export default App;
-
 const Main = styled.div`
   width: 100vw;
 `;
+
+export default App;
